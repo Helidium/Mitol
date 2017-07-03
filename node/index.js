f76d0e5b6bef7e6dd512d1b2956e3ecb2e8783e0
Helidium <helidium@gmail.com>
2017-07-03 10:54:58 +0200

    Changed module name load to mitol, that is published as npm module

--- node/index.js
+++ node/index.js
@@ -3,8 +3,8 @@
  */
 module.exports = (function() {
     "use strict";
-    try {
-        return require(`./mns_${process.platform}_${process.versions.modules}`);
+    try { // NODE-GYP
+        return require(`./bin/mitol`);
     } catch (e) {
         throw new Error('Error loading MitolServer module');
     }
